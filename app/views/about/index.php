07f509b76d9ce5e826ffcc39b75ad9cc4644c8dd
Riskiwahyudi1 <133681297+Riskiwahyudi1@users.noreply.github.com>
2024-02-09 20:57:05 +0700

    menghubungkan ke boostrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container mt-3">
+    <img src="<?= BASEURL;  ?>/img/profil.png" width="200" class="rounded-circle shadow" alt="">
     <h1>About me</h1>
     <h4>hallo nama saya <?= $data['nama']?>, saya seorang <?= $data['pekerjaan'] ?>, dan umur saya masih <?= $data['umur'] ?></h4>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,4 @@
-
+<div class="container mt-3">
     <h1>Ini adalah halaman home dari web saya</h1>
+
+</div>
